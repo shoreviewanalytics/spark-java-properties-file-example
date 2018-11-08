be22a527592cb646b78d6c106a83c58109397b80
shoreviewanalytics <chad.downey@shoreviewanalytics.com>
2018-11-08 05:39:04 +0000

    1

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-# spark-properties-file-example
+
+This example uses a .properties file with a spark job. It can be run on a single node DataStax Enterprise (DSE) cluster with Analytics enabled or a multi-node (DSE) cluster with Analytics enabled. In order to run this example its necessary to use the configuration and data file included in the repository.  
+
+A properties file allows you to pass various configuration values to the job at runtime.  Similar to spark-logback-example-two job, this job doesn't require the use of --driver-java-options "=Dlogback.configurationFile=" option with spark-submit as found in spark-logback-example-one.  The reference to a specific logback.xml file can be shared between jobs or you could create custom logback.xml file for each job using a name like application_name.logback.xml.  The logback.xml file is configured with a parameter ${jobname}, which takes a value set within the job using System.setProperty to provide the job name.  The job name is set in the .properties file.   
+
+To run:
+
+dse -u cassandra -p yourpassword spark-submit --class com.java.spark.SparkPropertiesFileExample /pathtojobsfolderonyourserver/spark-properties-file-example-0.1.jar
+
+Configuration and Data Files:
+
+Configuration and data files are located in the src/main/resources folder.  You will need to place these files in the appropriate location based upon your development and server environment.  
---
+++ src/main/java/com/java/spark/SparkPropertiesFileExample.java
@@ -0,0 +1,136 @@
+package com.java.spark;
+
+import org.apache.spark.api.java.JavaPairRDD;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.sql.SparkSession;
+import java.util.Arrays;
+import java.util.Properties;
+
+import org.apache.spark.SparkConf;
+
+import org.slf4j.LoggerFactory;
+import org.slf4j.Logger;
+import ch.qos.logback.classic.LoggerContext;
+import ch.qos.logback.classic.joran.JoranConfigurator;
+import ch.qos.logback.core.joran.spi.JoranException;
+import ch.qos.logback.core.util.StatusPrinter;
+
+import scala.Tuple2;
+import java.util.regex.Pattern;
+
+import java.io.FileInputStream;
+
+
+public class SparkPropertiesFileExample {
+	
+    private static final Logger LOGGER = LoggerFactory.getLogger(SparkPropertiesFileExample.class);
+	
+	public static void main(String[] args) {
+			 
+		Pattern SPACE = Pattern.compile(" ");
+		
+		/*
+		 * run the job within a basic try / catch block, so issues can be discovered and written to the log 
+		 */
+		try {  
+			
+			Properties appProps = new Properties();
+			/*
+			 * set the directory for the .properties file.   
+			*/
+			String rootPath = Thread.currentThread().getContextClassLoader().getResource("").getPath();
+			/*
+			 * set the complete path to the .properties file.  
+			 */
+			
+			String appConfigPath = rootPath + "config/spark-properties-file-example.properties";
+			
+			appProps.load(new FileInputStream(appConfigPath));	
+			/*
+			 * get the job name from the .properties file for the job
+			 */
+		
+			String jobName = appProps.getProperty("app3");
+			/*
+			 * get the path to the logback.xml file
+			 */
+			
+			String logbackfile = appProps.getProperty("logbackfile");
+			
+			/*
+			 * set the name of the job to be used in logback.xml
+			 */
+			System.setProperty("jobname",jobName);
+			
+			/*
+			 * set the logger context so you can use job specific logging throughout job processing
+			 */
+			LoggerContext context = (LoggerContext) LoggerFactory.getILoggerFactory();
+		    
+		    try {
+		      JoranConfigurator configurator = new JoranConfigurator();
+		      configurator.setContext(context);
+		      context.reset(); 
+		      configurator.doConfigure(logbackfile);
+		    } catch (JoranException je) {
+		      // StatusPrinter below will handle 
+		    }
+		    /*
+		     * this line catches any issues from the above try catch
+		     */
+		    StatusPrinter.printInCaseOfErrorsOrWarnings(context);
+		    
+		    
+		    LOGGER.info("The root path for the job is " + rootPath);
+		    LOGGER.info("The full path to the spark-properties-file-example.properties file is " + appConfigPath);
+		    LOGGER.info("Logback file location. " + logbackfile);
+		    LOGGER.info("Job name is " + jobName);
+		     
+		   			
+			SparkConf conf = new SparkConf().setAppName("LoggerSample");
+			JavaSparkContext sc = new JavaSparkContext(conf);
+			SparkSession spark = SparkSession.builder().appName("LoggerSample").getOrCreate();
+			SysStreamsLogger.bindSystemStreams();
+
+			LOGGER.info("Sample Info Logging: Starting up the job.");
+
+			/**
+			 * to test against a distributed environment place a file named words.csv on each node.
+			 * 
+			 */
+			JavaRDD<String> lines = spark.read().textFile("file:///home/one/data/words.csv").javaRDD();
+
+			JavaRDD<String> words = lines.flatMap(s -> Arrays.asList(SPACE.split(s)).iterator());
+
+			LOGGER.info("Sample Info Logging of transformation of words after lines.flatMap.");
+
+			words.take(5).forEach(System.out::println);
+
+			JavaPairRDD<String, Integer> ones = words.mapToPair(s -> new Tuple2<>(s, 1));
+
+			LOGGER.info("Sample Info Logging of transformation of ones RDD after being set to words.mapToPair.");
+
+			ones.take(5).forEach(System.out::println);
+
+			JavaPairRDD<String, Integer> counts = ones.reduceByKey((i1, i2) -> i1 + i2);
+
+			LOGGER.info("Sample Info Logging of the RDD - counts.toDebugString() " + counts.toDebugString());
+
+			// sample info logging of transformation using SysStreamsLogger
+			LOGGER.info("Sample Info Logging: Print out the first five key / value pairs of counts RDD");
+
+			counts.take(5).forEach(System.out::println);			 
+
+			sc.close();
+
+						
+		}catch(Exception e){
+			
+			LOGGER.error("An Error has occurred: Exiting the job. " + e.getMessage());
+		}
+				
+		
+	}
+
+}
